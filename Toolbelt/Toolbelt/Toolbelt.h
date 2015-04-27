f5dbfb6b5f784ae5e2b339119d9c6b64411ee5eb
Alexander Edge <alex@alexedge.co.uk>
2015-04-27 19:26:27 +0100

    Wishful thinking

--- Toolbelt/Toolbelt/Toolbelt.h
+++ Toolbelt/Toolbelt/Toolbelt.h
@@ -6,7 +6,7 @@
 //  Copyright (c) 2015 Alexander Edge. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
 
 //! Project version number for Toolbelt.
 FOUNDATION_EXPORT double ToolbeltVersionNumber;
