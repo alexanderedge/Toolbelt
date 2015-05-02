e49b24ab9afcedd9fadf843ae994dd0017d20d3c
Alexander Edge <alex@alexedge.co.uk>
2015-05-02 18:24:42 +0100

    Add validation extensions for phone number and email

---
+++ Toolbelt/Toolbelt/Validation.swift
@@ -0,0 +1,46 @@
+//
+//  Validation.swift
+//  Toolbelt
+//
+//  The MIT License (MIT)
+//
+//  Copyright (c) 02/05/2015 Alexander G Edge
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
+
+import Foundation
+
+extension String {
+    
+    public func validateWithRegexPattern(regex : String) -> Bool {
+        return NSPredicate(format: "SELF MATCHES %@", argumentArray: [regex]).evaluateWithObject(self)
+    }
+    
+    // This uses built-in iOS data detection.
+    // You might want something more powerful such as libPhoneNumber
+    // https://github.com/iziz/libPhoneNumber-iOS
+    public func isValidPhoneNumber () -> Bool {
+        return NSDataDetector(types: NSTextCheckingType.PhoneNumber.rawValue, error: nil)?.numberOfMatchesInString(self, options: nil, range: NSMakeRange(0, count(self))) > 0
+    }
+    
+    public func isValidEmail () -> Bool {
+        return self.validateWithRegexPattern(".+@([A-Za-z0-9]+\\.)+[A-Za-z]{2}[A-Za-z]*")
+    }
+    
+}
