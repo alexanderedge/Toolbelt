1bbee8760499dce64f2a739b9e7418ba32052482
Alexander Edge <alex@alexedge.co.uk>
2015-05-02 19:18:51 +0100

    Add localisations and change Path.swift to Filepath.swift

---
+++ Toolbelt/Toolbelt/Filepath.swift
@@ -0,0 +1,56 @@
+//
+//  Filepath.swift
+//  Toolbelt
+//
+//  The MIT License (MIT)
+//
+//  Copyright (c) 02/05/2015 Alexander G Edge
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
+
+import Foundation
+
+extension String {
+    
+    public static func temporaryFilePathWithExtension(ext : String?) -> String {
+        let path = NSTemporaryDirectory().stringByAppendingPathComponent(NSUUID().UUIDString)
+        if (ext != nil) {
+            return path.stringByAppendingPathExtension(ext!)!
+        } else {
+            return path
+        }
+    }
+    
+    public static func temporaryFilePath () -> String {
+        return temporaryFilePathWithExtension(nil)
+    }
+    
+}
+
+extension NSURL {
+    
+    public class func temporaryFileURLWithExtension(ext : String?) -> NSURL {
+        return NSURL(string: String.temporaryFilePathWithExtension(ext))!
+    }
+    
+    public class func temporaryFileURL() -> NSURL {
+        return NSURL(string: String.temporaryFilePath())!
+    }
+    
+}
