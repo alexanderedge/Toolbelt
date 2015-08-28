a5216a6758b7bd19a0e3da6ec5eef9ba93f72abe
Alexander Edge <alex@alexedge.co.uk>
2015-08-28 09:44:25 +0100

    Fix compilation on OSX

--- Toolbelt/Toolbelt/Contacts.swift
+++ Toolbelt/Toolbelt/Contacts.swift
@@ -1,5 +1,5 @@
 //
-//  String.swift
+//  Contacts.swift
 //  Toolbelt
 //
 //  Created by Alexander G Edge on 07/08/2015.
@@ -9,8 +9,30 @@
 import Contacts // iOS 9
 import AddressBook // iOS 8
 
+@available(iOS 2.0, OSX 10.11, *)
 public func NSLocalizedStringFromName(givenName : String?, familyName : String?) -> String {
-    if #available(iOS 9.0, OSX 10.11, *) {
+    
+    #if os(iOS)
+        if #available(iOS 9.0, *) {
+            let contact = CNMutableContact()
+            if let name = givenName {
+                contact.givenName = name
+            }
+            if let name = familyName {
+                contact.familyName = name
+            }
+            return CNContactFormatter.stringFromContact(contact, style: .FullName)!
+        } else {
+            let record = ABPersonCreate().takeRetainedValue()
+            if let name = givenName {
+                ABRecordSetValue(record, kABPersonFirstNameProperty, name, nil)
+            }
+            if let name = familyName {
+                ABRecordSetValue(record, kABPersonLastNameProperty, name, nil)
+            }
+            return ABRecordCopyCompositeName(record).takeRetainedValue() as String
+        }
+    #else
         let contact = CNMutableContact()
         if let name = givenName {
             contact.givenName = name
@@ -19,14 +41,7 @@ public func NSLocalizedStringFromName(givenName : String?, familyName : String?)
             contact.familyName = name
         }
         return CNContactFormatter.stringFromContact(contact, style: .FullName)!
-    } else {
-        let record = ABPersonCreate().takeRetainedValue()
-        if let name = givenName {
-            ABRecordSetValue(record, kABPersonFirstNameProperty, name, nil)
-        }
-        if let name = familyName {
-            ABRecordSetValue(record, kABPersonLastNameProperty, name, nil)
-        }
-        return ABRecordCopyCompositeName(record).takeRetainedValue() as String
-    }
+    #endif
+    
+    
 }
