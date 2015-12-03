af69c63c960679d72f9a845608f8c486db13154b
Alexander Edge <alex@alexedge.co.uk>
2015-12-03 14:54:38 +0000

    Remove Contacts from Toolbelt Mac

--- Toolbelt/Toolbelt/Contacts.swift
+++ Toolbelt/Toolbelt/Contacts.swift
@@ -9,30 +9,10 @@
 import Contacts // iOS 9
 import AddressBook // iOS 8
 
-@available(iOS 2.0, OSX 10.11, *)
+@available(iOS 2.0, *)
 public func NSLocalizedStringFromName(givenName : String?, familyName : String?) -> String {
     
-    #if os(iOS)
-        if #available(iOS 9.0, *) {
-            let contact = CNMutableContact()
-            if let name = givenName {
-                contact.givenName = name
-            }
-            if let name = familyName {
-                contact.familyName = name
-            }
-            return CNContactFormatter.stringFromContact(contact, style: .FullName)!
-        } else {
-            let record = ABPersonCreate().takeRetainedValue()
-            if let name = givenName {
-                ABRecordSetValue(record, kABPersonFirstNameProperty, name, nil)
-            }
-            if let name = familyName {
-                ABRecordSetValue(record, kABPersonLastNameProperty, name, nil)
-            }
-            return ABRecordCopyCompositeName(record).takeRetainedValue() as String
-        }
-    #else
+    if #available(iOS 9.0, *) {
         let contact = CNMutableContact()
         if let name = givenName {
             contact.givenName = name
@@ -41,8 +21,16 @@ public func NSLocalizedStringFromName(givenName : String?, familyName : String?)
             contact.familyName = name
         }
         return CNContactFormatter.stringFromContact(contact, style: .FullName)!
-    #endif
-    
+    } else {
+        let record = ABPersonCreate().takeRetainedValue()
+        if let name = givenName {
+            ABRecordSetValue(record, kABPersonFirstNameProperty, name, nil)
+        }
+        if let name = familyName {
+            ABRecordSetValue(record, kABPersonLastNameProperty, name, nil)
+        }
+        return ABRecordCopyCompositeName(record).takeRetainedValue() as String
+    }
     
 }
 
