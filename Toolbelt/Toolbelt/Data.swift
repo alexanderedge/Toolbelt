3d23213ebf4fe9546715a5bab6f2f29d99ee9350
Alexander Edge <alex@alexedge.co.uk>
2016-02-25 14:08:42 +0000

    Use `reduce` for hex string implementation

--- Toolbelt/Toolbelt/Data.swift
+++ Toolbelt/Toolbelt/Data.swift
@@ -11,13 +11,9 @@ import Foundation
 extension NSData {
     
     public var hexadecimalString : String {
-        var str = String()
         var bytes = [UInt8](count: self.length, repeatedValue: 0)
         self.getBytes(&bytes, length: self.length)
-        for byte in bytes {
-            str += String(format: "%02x",byte)
-        }
-        return str
+        return bytes.reduce(""){ return $0 + String(format:"%02x",$1)}
     }
     
 }
